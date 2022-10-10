76e7cf5d644d097997deb7361e8b7d84dd87fec1
Truong Nguyen <meomeo0308@gmail.com>
2022-10-10 09:50:41 +0700

    ok

---
+++ storage/framework/views/b987e44ca7523a717aade0363ac5ae01ee1d8752.php
@@ -0,0 +1,4 @@
+<div class="form-group mb-3">
+    <p><?php echo e(trans('plugins/contact::contact.shortcode_content_description')); ?></p>
+</div>
+<?php /**PATH /Users/macos/workspace/botble/platform/plugins/contact/resources/views//partials/short-code-admin-config.blade.php ENDPATH**/ ?>
